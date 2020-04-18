323f9e7890993caa2581dc22114b0e67d668e580
Roberto Padilla <robert.ipo@hotmail.com>
2020-04-18 17:44:14 -0500

    variable entorno heroku

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,7 @@
 process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
-let urlDB = 'mongodb+srv://robrig:fmeGZf4b4mRCkkBJ@cluster0-zfr54.mongodb.net/cafe';
+let urlDB = process.env.MONGO_URI;
 
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
